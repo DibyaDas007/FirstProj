a071d3cc149ac6c44d103cbbc4ede4fcc32c2612
Dibya <Dibya@192.168.1.4>
2019-03-03 23:44:50 +0530

    Spring Conf

---
+++ src/main/java/com/proj/MyApplication.java
@@ -0,0 +1,23 @@
+package com.proj;
+
+import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+@SpringBootApplication
+public class MyApplication {
+	
+	
+		public static void main(String[] args) throws JsonGenerationException, IOException {
+			SpringApplication.run(MyApplication.class, args);
+	
+
+		System.out.println("Hello");
+	}
+}
+
